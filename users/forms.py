35df6b86dbd55e076288f94cc6ee85331e51f1cb
Oleg Sokolov <olegnim@gmail.com>
2021-01-22 15:02:48 +0300

    PEP8 corrections

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import ModelForm
-
-from .models import Contact
 
 User = get_user_model()
 
@@ -11,9 +8,3 @@ User = get_user_model()
 class CreationForm(UserCreationForm):
     model = User
     fields = ('first_name', 'last_name', 'username', 'email')
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ('name', 'email', 'subject', 'body')
